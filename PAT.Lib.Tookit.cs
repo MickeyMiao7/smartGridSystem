1c8e97178463d2b352c2dcc379c71958ea042830
molly <kicy24@outlook.com>
2017-04-16 14:05:26 +0800

    the first version of C# scripts Apr 16

---
+++ PAT.Lib.Tookit.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+//the namespace must be PAT.Lib, the class and method names can be arbitrary
+namespace PAT.Lib
+{
+    /// <summary>
+    /// The math library that can be used in your model.
+    /// all methods should be declared as public static.
+    /// 
+    /// The parameters must be of type "int", or "int array"
+    /// The number of parameters can be 0 or many
+    /// 
+    /// The return type can be bool, int or int[] only.
+    /// 
+    /// The method name will be used directly in your model.
+    /// e.g. call(max, 10, 2), call(dominate, 3, 2), call(amax, [1,3,5]),
+    /// 
+    /// Note: method names are case sensetive
+    /// </summary>
+    public class Toolkit
+    {
+    	public static int RandNumGenerator()
+        {
+        	Random ro = new Random();  
+        	int iResult;  
+　　		int iUp = 100;  
+　　		iResult = ro.Next(iUp);
+　　		return iResult;
+        }
+        
+        // extend calculate/ accept/ reject and seal, return hashvalue.        
+        public static string HashFunction(string old_hash, string operation)
+        {
+            // extend calculate price
+            if (operation == "calculate"){
+                // calculate *7 hash
+                String toHash = old_hash + "*7";
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] data = System.Text.Encoding.Default.GetBytes(toHash);   
+                byte[] md5data = md5.ComputeHash(data);  
+                md5.Clear();  
+                string mid = "";  
+                for (int i = 0; i < md5data.Length - 1; ++i)  
+                {  
+                    mid += md5data[i].ToString("x").PadLeft(2, '0');  
+                }  
+                // calculate +12 hash
+                mid += "+12";
+                data = System.Text.Encoding.Default.GetBytes(mid); 
+                md5data = md5.ComputeHash(data);  
+                md5.Clear();  
+                string ans = "";  
+                for (int i = 0; i < md5data.Length - 1; ++i)  
+                {  
+                    ans += md5data[i].ToString("x").PadLeft(2, '0');  
+                } 
+                return ans;
+            }
+            // other operations
+            // seal / accept / reject
+            else {
+                String toHash = old_hash + operation;
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] data = System.Text.Encoding.Default.GetBytes(toHash);   
+                byte[] md5data = md5.ComputeHash(data);  
+                md5.Clear();  
+                string ans = "";  
+                for (int i = 0; i < md5data.Length - 1; ++i)  
+                {  
+                    ans += md5data[i].ToString("x").PadLeft(2, '0');  
+                }  
+                return ans;
+            }
+        }
+
+        // extend nonce, return the first hash value
+        public static string HashFunction(int nonce)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();  
+            byte[] data = System.Text.Encoding.Default.GetBytes(nonce.ToString());   
+            byte[] md5data = md5.ComputeHash(data);  
+            md5.Clear();  
+            string ans = "";  
+            for (int i = 0; i < md5data.Length - 1; ++i)  
+            {  
+                ans += md5data[i].ToString("x").PadLeft(2, '0');  
+            }  
+            return ans;
+        }
+        
+        
+        // calculate the price 
+        public static int CalculatePrice(int usage)
+        {
+        	int price = 1;
+        	price = usage * 7 + 12;	
+        	return price;
+        }
+        
+        public static string[] GenerateKeys()
+        {
+        	string[] keys = new String[2];
+        	RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+        	keys[0] = rsa.ToXmlString(true);
+        	keys[1] = rsa.ToXmlString(false);
+        	return keys;
+        }
+    }
+}
